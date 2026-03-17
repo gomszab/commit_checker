2a0abf9e6706cf802d010dd5e96ba9680ea72915
BenceEgi <bencemarcell08@gmail.com>
2026-03-17 14:26:06 +0100

    errors

--- src/api/error_handler.rs
+++ src/api/error_handler.rs
@@ -1,7 +1,37 @@
 use colored::Colorize;
+use crate::api::FileContext;
 
-pub struct ErrorHandler{}
-impl ErrorHandler {
+pub struct ErrorHandler<'a>{
+    file_contexts: Vec<&'a mut FileContext<'a>>,
+    errored_files: Vec<(&'a str, Vec<String>)>,
+    ok_files: Vec<&'a str>
+}
+impl<'a> ErrorHandler<'a> {
+    pub fn new() -> Self{
+        let err_handler = ErrorHandler{
+            file_contexts: Vec::new(),
+            errored_files: Vec::new(),
+            ok_files: Vec::new()
+        };
+        err_handler
+    }
+
+    pub fn subscribe(&mut self, file_context: &'a mut FileContext<'a>){
+        self.file_contexts.push(file_context);
+    }
+
+    pub fn show_errors(&self){
+
+    }
+
+    pub fn summa(&self){
+
+    }
+
+    fn gather_handler_results(&self){
+
+    }
+
     pub fn print_errors(errors: Vec<String>) {
         for error in errors {
             eprintln!("{}", error.red());
